bcf5f735d2fae893f1d7bb175bdaada3483596dd
stefano.morroni <smorroni@hotmail.com>
2024-03-03 18:56:12 +0100

    GeoCatalogo con SidebarMenu

--- web/client/plugins/GeoCatalogo.jsx
+++ web/client/plugins/GeoCatalogo.jsx
@@ -13,6 +13,7 @@ export default createPlugin('GeoCatalogo', {
         BurgerMenu: {
             name: 'GeoCatalogo',
             position: 1100,
+            tooltip: 'GeoCatalogo',
             text: 'GeoCatalogo',
             icon: <Glyphicon glyph="folder-open"/>, 
             action: () => ({type: ''}),
@@ -22,6 +23,19 @@ export default createPlugin('GeoCatalogo', {
             },
             priority: 2,
             doNotHide: true
+        }, SidebarMenu: {
+            name: 'GeoCatalogo',
+            position: 1100,
+            tooltip: 'GeoCatalogo',
+            icon: <Glyphicon glyph="folder-open"/>, 
+            action: () => ({type: ''}),
+            selector: (state, ownProps) => {
+                const docsUrl = get(ownProps, 'docsUrl', 'https://mapstore.readthedocs.io/en/latest/');
+                return {href: docsUrl, target: '_blank'};
+            },
+            priority: 2,
+            doNotHide: true,
+            toggle: true
         }
     }
 });
